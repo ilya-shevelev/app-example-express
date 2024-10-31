294fbd89d2490ec0f28a02db2b66fa3d0bc9aa70
i.shevelev <i.shevelev@timeweb.ru>
2024-10-31 15:47:21 +0300

    test .ts MIME

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Timeweb Cloud + Express = ️ ❤️')
-})
+app.get('/src/main.ts', (req, res) => {
+  const filePath = path.join(__dirname, 'src', 'main.ts');
+  res.sendFile(filePath); // Отправляем файл без указания MIME-типа
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
